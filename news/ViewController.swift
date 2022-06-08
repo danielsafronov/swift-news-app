350f41b2f0a60c0b620c95e178fed4cc27778a6a
Daniel <iamdanielsafronov@gmail.com>
2022-06-08 14:39:11 +0300

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Daniel Safronov on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
